70d544f27a50605152b06218fbdf04b5f0679f2f
istimaldar <istimaldar@gmail.com>
2023-04-25 01:12:32 +0400

    fix(nixos): Unfree packages allowed

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
+        config.allowUnfree = true;
       };
     in {
       nixosConfigurations.kionithar = nixpkgs.lib.nixosSystem {
