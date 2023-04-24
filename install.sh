4c881edec6a6e8412d96f79fd2337a174b0f4c8f
istimaldar <istimaldar@gmail.com>
2023-04-24 20:12:03 +0400

    feat(install): Partition formatting added

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT=0
-FORMAT=0
-SWAP=8
+set -exuo pipefail
+
+export SCRIPT=0
+export FORMAT=0
+export SWAP=8
 
 usage() {
     cat <<HELP
@@ -21,6 +23,7 @@ HELP
     exit 2;
 }
 
+# Parse argumets
 ARGUMENTS=$(getopt -n install.sh -o sfd:b:i:w: \
 -l script,format,device:,boot-partition:,install-partition:,swap: -- "$@")
 
@@ -33,18 +36,19 @@ eval set -- "$ARGUMENTS"
 while :;
 do
   case $1 in
-    -s | --script)            SCRIPT=1                ; shift;    ;;
-    -f | --format)            FORMAT=1                ; shift;    ;;
-    -d | --device)            DEVICE="$2"             ; shift 2;  ;;
-    -b | --boot-partition)    BOOT_PARTITION="$2"     ; shift 2;  ;;
-    -i | --install-partition) INSTALL_PARTITION="$2"  ; shift 2;  ;;
-    -w | --swap)              SWAP="$2"               ; shift 2;  ;;
-    --)                       shift                   ; break;    ;;
+    -s | --script)            export SCRIPT=1                ; shift;    ;;
+    -f | --format)            export FORMAT=1                ; shift;    ;;
+    -d | --device)            export DEVICE="$2"             ; shift 2;  ;;
+    -b | --boot-partition)    export BOOT_PARTITION="$2"     ; shift 2;  ;;
+    -i | --install-partition) export INSTALL_PARTITION="$2"  ; shift 2;  ;;
+    -w | --swap)              export SWAP="$2"               ; shift 2;  ;;
+    --)                       shift                          ; break;    ;;
     *) echo "Unexpected option: $1 - this should not happen."
        usage ;;
   esac
 done
 
+# Validate arguments
 if [[ "$FORMAT" == "1" && -z "$DEVICE" ]];
 then
   echo "If you want to install with device formatting you must specify the device.";
@@ -58,3 +62,11 @@ then
   usage;
 fi
 
+# Format device
+if [[ "$FORMAT" == "1" ]];
+then
+  parted "$DEVICE" -- mklabel gpt
+  parted "$DEVICE" -- mkpart ESP fat32 1MB 512MB
+  parted "$DEVICE" -- set 1 esp on
+  parted "$DEVICE" -- mkpart primary 512MB 100%
+fi
