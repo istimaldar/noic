ab33edc9eb5a6883b5734f10ece6eff5df9de0b7
istimaldar <istimaldar@gmail.com>
2023-04-24 19:53:32 +0400

    feat(install): Installation arguments validation added

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+SCRIPT=0
+FORMAT=0
+SWAP=8
+
 usage() {
     cat <<HELP
 Usage:
@@ -21,7 +25,36 @@ ARGUMENTS=$(getopt -n install.sh -o sfd:b:i:w: \
 -l script,format,device:,boot-partition:,install-partition:,swap: -- "$@")
 
 ARGUMENTS_VALID=$?
-if [ "$ARGUMENTS_VALID" != "0" ]; then
+if [[ "$ARGUMENTS_VALID" != "0" ]]; then
   usage
+fi
+
+eval set -- "$ARGUMENTS"
+while :;
+do
+  case $1 in
+    -s | --script)            SCRIPT=1                ; shift;    ;;
+    -f | --format)            FORMAT=1                ; shift;    ;;
+    -d | --device)            DEVICE="$2"             ; shift 2;  ;;
+    -b | --boot-partition)    BOOT_PARTITION="$2"     ; shift 2;  ;;
+    -i | --install-partition) INSTALL_PARTITION="$2"  ; shift 2;  ;;
+    -w | --swap)              SWAP="$2"               ; shift 2;  ;;
+    --)                       shift                   ; break;    ;;
+    *) echo "Unexpected option: $1 - this should not happen."
+       usage ;;
+  esac
+done
+
+if [[ "$FORMAT" == "1" && -z "$DEVICE" ]];
+then
+  echo "If you want to install with device formatting you must specify the device.";
+  usage;
+fi
+
+
+if [[ "$FORMAT" == "0" && ( -z "$BOOT_PARTITION" || -z "$INSTALL_PARTITION") ]];
+then
+  echo "If you want to install without device formatting you must specify both boot and install partitions.";
+  usage;
 fi
 
