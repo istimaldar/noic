a0f6b1913585784d8f7f701a8e35fa01761788be
istimaldar <istimaldar@gmail.com>
2023-05-16 18:57:23 +0400

    feat(nixos): OpenVPN Quick access

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -19,6 +19,11 @@
         mkdir -p $HOME/.kubedata/udev
         '';
     };
+    shellAliases = {
+      vpna5 = "openvpn3 session-start --config $HOME/.openvpn/a5labs.ovpn";
+      vpnka = "openvpn3 session-start --config $HOME/.openvpn/kashxa.ovpn";
+      vpnno = "openvpn3 sessions-list | grep Path | awk '{ print $2 }' | xargs -I{} openvpn3 session-manage -D --session-path {}";
+    };
     stateVersion = "22.11";
   };
 
