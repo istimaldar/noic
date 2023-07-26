1f0253e23732da425951a4f0a11bb22d211a82e8
istimaldar <istimaldar@gmail.com>
2023-07-26 11:23:57 +0400

    feature(nixos): Auto-clean and update

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -92,10 +92,16 @@
   nix = {
     extraOptions = ''
     experimental-features = nix-command flakes
+    min-free = ${toString (10 * 1024 * 1024 * 1024)}
+    max-free = ${toString (25 * 1024 * 1024 * 1024)}
     '';
     gc = {
       automatic = true;
-      dates = "Fri 22:00:00";
+      dates = "daily";
+      options = "--delete-older-than 3d";
+    };
+    settings = {
+      auto-optimise-store = true;
     };
   };
 
