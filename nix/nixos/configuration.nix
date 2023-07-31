aede3213545140499ac3a608bc85a404a51bdb4c
istimaldar <istimaldar@gmail.com>
2023-07-31 10:37:11 +0400

    feature(nixos): Steam installed

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -113,10 +113,17 @@
         enable = true;
         hidpi = true;
       };
+
     };
 
     zsh = {
       enable = true;
+    };
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
 
     openvpn3 = {
