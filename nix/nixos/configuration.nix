2ce4bacbd9ca2d462f7446074d74b12a856620f8
istimaldar <istimaldar@gmail.com>
2023-11-20 14:22:27 +0400

    feature: Ingress removed

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -144,7 +144,6 @@
       serverAddr = if host.features.kubernetes.ha && !host.features.kubernetes.server then "https://192.168.100.5:6443" else "";
       extraFlags = if host.features.kubernetes.server then toString [
         "--write-kubeconfig-mode=644"
-        "--disable=traefik"
       ] else "";
     };
     u9fs = {
