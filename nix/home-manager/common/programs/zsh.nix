293ccfc63cf0d631e0b490ce3f718b1a37ba103a
istimaldar <istimaldar@gmail.com>
2024-08-02 19:29:24 +0400

    fix: ZSH history search fixed

--- nix/home-manager/common/programs/zsh.nix
+++ nix/home-manager/common/programs/zsh.nix
@@ -8,6 +8,14 @@
   };
   historySubstringSearch = {
     enable = true;
+    searchUpKey = [
+      "^[[A"
+      "$terminfo[kcuu1]"
+    ];
+    searchDownKey = [
+      "^[[B"
+      "$terminfo[kcud1]"
+    ];
   };
   enableCompletion = true;
   initExtra = ''
